ed7a0493f463f97714fe6382d3b93c44060be9af
bufOvf <66708776+bufOvf@users.noreply.github.com>
2024-10-22 06:08:04 +1100

    refactor main

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from urllib import response
+# from urllib import response
 from dotenv import load_dotenv
 import discord
 import os 
@@ -8,12 +8,14 @@ from ai import initialise_groq, groq_response, reload_ai_config, log, send_syste
 from datetime import datetime
 
 
+
 # bot permissions: administrator
 
 # load configs
 load_dotenv()
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 TOKEN = os.getenv("TOKEN")
+
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -45,14 +47,14 @@ def load_and_apply_config():
 @client.event
 async def on_ready():
     client.user.global_name = 'Mira'
-    ready_channel = client.get_channel(channel_id[0])
+    ready_channel = client.get_channel(channel_id[1])
     await initialise_groq(GROQ_API_KEY)
     # client.loop.create_task(console_listener())
 
     
 
     if not asleep:
-        await ready_channel.send('{Mira is back... }')
+        await ready_channel.send('{Mira is online... }')
         # messages_context = [message async for message in ready_channel.history(limit=5)]
         # messages_context = [f'[{message.author.nick}: {message.content}],' if message.author.nick else f'[Mira: {message.content}],' for message in messages_context]
         # messages_context = 'PREVIOUS MESSAGES: \n{'+'\n'.join(messages_context)+'}'
@@ -70,7 +72,7 @@ async def on_message(message):
     if message.channel.id not in channel_id or message.author == client.user or not userAllowed:
         return
     
-    if message.content.startswith('$'):
+    if message.content.startswith('!'):
         log(f'{message.author.name} ran command: {message.content}')
         try:    
             await commands[message.content](message)
@@ -80,12 +82,12 @@ async def on_message(message):
         await send_message(message)
 
 async def command_help(message):
-    await message.channel.send('{Commands: $help, $wakeup, $init, $reloadai, $reloaddiscord, $reloadtts, $sleep, $die, $join}')
+    await message.channel.send('`Commands: !help, !wakeup, !init, !reloadai, !reloaddiscord, !reloadtts, !sleep, !die, !join}`')
 
 async def command_wakeup(message):
     global asleep
     asleep = False
-    await message.channel.send('{Mira is awake..}')
+    await message.channel.send('`Mira is awake..`')
 
 async def command_init(message):
     log(f'Initialising groq {message.author.name}')
@@ -93,20 +95,20 @@ async def command_init(message):
 
 async def command_reloadai(message):
     if await reload_ai_config():
-        await message.channel.send('{AI config updated}')
+        await message.channel.send('`AI config updated`')
 
 async def command_reloaddiscord(message):
     if await reload_tts_config():
-        await message.channel.send('{Discord config updated}')
+        await message.channel.send('`Discord config updated`')
     load_and_apply_config()
     
 async def command_sleep(message):
     global asleep
     asleep = True
-    await message.channel.send('{Mira is sleeping..}')
+    await message.channel.send('`Mira is sleeping..`')
 
 async def command_die(message):
-    await message.channel.send('{Mira has left..}')
+    await message.channel.send('`Mira has left..`')
     log('Discord bot killed')
     await client.close()
 
@@ -134,9 +136,9 @@ async def console_listener():
 # voice channel stuff
 async def command_reloadtts(message):
     if await reload_tts_config():
-        await message.channel.send('{TTS config updated}')
+        await message.channel.send('`TTS config updated`')
     else:
-        await message.channel.send('{TTS config failed to update}')
+        await message.channel.send('`TTS config failed to update`')
 
 
 async def command_joinvc(message):
@@ -163,16 +165,16 @@ async def play_tts(message):
 
 
 commands = {
-      '$help': command_help,
-      '$wakeup': command_wakeup,
-      '$init': command_init,
-      '$reloadai': command_reloadai,
-      '$reloaddiscord': command_reloaddiscord,
-      '$reloadtts': command_reloadtts,
-      '$sleep': command_sleep,
-      '$join': command_joinvc,
-      '$leave': command_leavevc,
-      '$die': command_die,
+      '!help': command_help,
+      '!wakeup': command_wakeup,
+      '!init': command_init,
+      '!reloadai': command_reloadai,
+      '!reloaddiscord': command_reloaddiscord,
+      '!reloadtts': command_reloadtts,
+      '!sleep': command_sleep,
+      '!join': command_joinvc,
+      '!leave': command_leavevc,
+      '!die': command_die,
   }
 
 
